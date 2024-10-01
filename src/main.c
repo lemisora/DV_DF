5bdd712da5cfc35462618a49a028e878ca62c23c
lemisora <leonardo.michel.dom@gmail.com>
2024-10-01 15:15:12 -0600

    Adición de pruebas para manejar correctamente los bits y hacer una traducción correcta

--- src/main.c
+++ src/main.c
@@ -1,11 +1,40 @@
 #include "tabla_paginas.h"
-#define TAM_TABLA 16
-#define N_MARCOS 4
-
-#define MASK_U_INT 1 << 31
-
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+// #define TAM_TABLA 16
+// #define N_MARCOS 4
+
+/* Datos importantes para el programa:
+    - Tamaño de página
+    - Número de páginas
+    - Número de marcos
+
+    D.V.
+    |No. página|Desplazamiento|
+
+    D.F.
+    |No. marco|Desplazamiento|
+
+    Entrada de tabla de páginas
+    |b_permiso|b_ref|b_mod|b_pres_aus|b_cache|No. marco|
+*/
+
+#define TAM_PAG 1024
+#define N_PAG 512
+#define N_MARCOS 32
+
+//Elementos de la dirección física
+#define BIT_N_MARCOS (int)log2(N_MARCOS) //5 bits
+//Desplazamiento 10 bits
+
+//Elementos de la dirección virtual
+#define BITS_N_PAG (int)log2(N_PAG) //9 bits
+#define BITS_DESP (int)log2(TAM_PAG) //10 bits
+
+#define MASK_U_INT 1 << 31
 
 void printBinary(unsigned int num) {
     unsigned int mask = MASK_U_INT;  // Comenzamos con el bit más significativo
@@ -27,37 +56,47 @@ void printBinary(unsigned int num) {
     printf("\n");
 }
 
-
 int main(){
-    table_entry_t ent1 = {  .b_permiso = 1,
-                            .b_ref = 1,
-                            .b_mod = 1,
-                            .b_pres_aus = 1,
-                            .b_cache = 1,
-                            .n_frame = 3};
-    // unsigned int test_bit = 10;
-    // printf("Valor de la variable con el bit de prueba: ");
-    // printBinary(test_bit);
-    unsigned int DV = 0;
-    ent1.b_permiso <<= TAM_TABLA;
-    ent1.b_ref <<= TAM_TABLA-1;
-    ent1.b_mod <<= TAM_TABLA-2;
-    ent1.b_pres_aus <<= TAM_TABLA-3;
-    ent1.b_cache <<= TAM_TABLA-4;
+    // const int tam_pag = 1024;
+    // const int num_pag = 512;
+    // const int num_marcos = 32;
+    // const int bits_desp = log2(tam_pag);
+    // //printf("Tamaño de desplazamiento: %d", (int)BIT_DESP);
+    unsigned int mask = 0;
+    unsigned int n_pag = 0; //Acá se almacenará el número de página
+    unsigned int n_marco = 0;   //Acá se almacenará el número de marco
+    unsigned int desp = 0;  //Acá se almacenará el valor de desplazamiento
 
-    DV = DV | ent1.b_permiso;
-    DV = DV | ent1.b_ref;
-    DV = DV | ent1.b_mod;
-    DV = DV | ent1.b_pres_aus;
-    DV = DV | ent1.b_cache;
-    DV = DV | ent1.n_frame;
-    printf("Valor de bit permiso: %u\n", ent1.b_permiso);
-    printf("Valor de bit referencia: %u\n", ent1.b_ref);
-    printf("Valor de bit modificada: %u\n", ent1.b_mod);
-    printf("Valor de bit presente/ausente: %u\n", ent1.b_pres_aus);
-    printf("Valor de bit de caché: %u\n", ent1.b_cache);
-    printf("Valor de DV: %u\n", DV);
-    printf("Valor de DV en binario: ");
+    //Para el ejemplo es de 19 bits
+    //Suponer la siguiente D.V. -> 0 0010 0000 10 0101 0010
+    //Donde n_pag = 32 y desp = 594
+    unsigned int DV = 33362;
+    printf("Dirección virtual: %u\tEn binario: ", DV);
     printBinary(DV);
+
+    //Para el ejemplo es de 15 bits
+    unsigned int DF = 0;
+
+    //Averiguar número de página de la d.v.
+    unsigned int DV_TEMP = DV;
+    DV_TEMP >>= BITS_DESP;
+    n_pag = DV_TEMP;
+
+    DV_TEMP <<= BITS_DESP;
+    printf("Valor de DV_TEMP después de recorrimiento: %u\n", DV_TEMP);
+    mask = DV_TEMP ^ UINT_MAX;
+    printf("Valor de mask después de enmascaramiento con XOR: %u\n", mask);
+    desp = DV & mask;
+
+    printf("N. pág: %u\tDesp: %u\n", n_pag, desp);
+
+    //Suponer que en la página 32 se tiene el número de marco 12
+    //Averiguar la dirección física con los datos obtenidos
+    n_marco = 12;
+    DF = n_marco << BITS_DESP;
+    DF |= desp;
+    //Debería resultar en 01100 10 0101 0010 que en decimal es: 12882
+    printf("Dirección físca: %u\tEn binario: ", DF);
+    printBinary(DF);
     return EXIT_SUCCESS;
 }
