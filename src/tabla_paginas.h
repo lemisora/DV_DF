9bbab36ab2fbc8a548ae4a02297cdd9c62b04bc1
lemisora <leonardo.michel.dom@gmail.com>
2024-10-01 15:24:02 -0600

    Se añade el tipo para tabla y se planean métodos para interactuar con la tabla

--- src/tabla_paginas.h
+++ src/tabla_paginas.h
@@ -1,15 +1,21 @@
+#include <stdlib.h>
 #ifndef TABLA_H
 #define TABLA_H
 
 typedef struct entrada table_entry_t;
+typedef table_entry_t* tabla;
 
 struct entrada{
-    int b_permiso;
-    int b_ref;
-    int b_mod;
-    int b_pres_aus;
-    int b_cache;
+    int b_permiso;  //Permiso de lectura/escritura, 0 solo lectura, 1 lectura/escritura
+    int b_ref;      //0 no accedida recientemente, 1 accedida recientemente
+    int b_mod;      //0 no modificada, 1 modificada
+    int b_pres_aus; //0 no está en memoria principal, 1 está en memoria principal
+    int b_cache;    //0 cache habilitado, 1 caché inhabilitado
     int n_frame;
 };
+
+int add_entry(tabla table, table_entry_t entrada, int index); //Función para añadir entradas a la tablaa
+int init_table(int size);   //Función para inicializar la tabla
+int translateVD_PD();   //Función para traducir memoria virtual a física
 
 #endif
