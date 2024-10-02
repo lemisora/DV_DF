767162e2d2f1c54ffeda56e298d57887321359a6
lemisora <leonardo.michel.dom@gmail.com>
2024-10-02 00:45:53 -0600

    Adición de métodos para: 	- Imprimir tabla de páginas 	- Mapear número de marcos con la dirección virtual Mejora en método de inicializar memoria

--- src/tabla_paginas.c
+++ src/tabla_paginas.c
@@ -1,19 +1,68 @@
 #include "tabla_paginas.h"
-//#include <stdio.h>
+#include "file_entries.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int add_entry(tabla table, table_entry_t entrada, int index){
     table[index] = entrada;
     return EXIT_SUCCESS;
 }
 
-// int init_table(tabla tabla_paginas, int size){
-//     if((tabla_paginas = malloc(sizeof(tabla)*size))==NULL){
-//         perror("No se pudo reservar memoria para la tabla de páginas");
-//         return EXIT_FAILURE;
-//     }
-//     for(int i = 0; i < size; i++){
-//         add_entry(tabla_paginas, NULL, i);
-//     }
-//     return EXIT_SUCCESS;
-// }
+int init_table(FILE* file, tabla* tabla_paginas, unsigned int size, unsigned int* tam_pag, unsigned int* num_pag, unsigned int* num_marcos){
+    *tabla_paginas = (tabla)malloc(sizeof(table_entry_t)*size);
+    if(tabla_paginas == NULL){
+        perror("No se pudo reservar memoria para la tabla de páginas");
+        fclose(file);
+        return EXIT_FAILURE;
+    }
+    /* Añadir entradas */
+    leer(file, tabla_paginas, size, tam_pag, num_pag, num_marcos);
+    fclose(file);
+    return EXIT_SUCCESS;
+}
+
+unsigned int getMarco(tabla t, unsigned int index){
+    unsigned int frame;
+    frame = t[index].n_frame;
+    return frame;
+}
+
+int translateVD_PD(tabla t, const int tam_pag, const int num_pag, const int num_marcos, const int bits_desp, unsigned int DV){
+    unsigned int DF = 0;    //Dirección física
+    unsigned int DV_TEMP = DV;  //Variable auxiliar para tratar con bits
+    unsigned int mask = 0;  //Máscara
+    unsigned int n_pag = 0; //Acá se almacenará el número de página
+    unsigned int n_marco = 0;   //Acá se almacenará el número de marco
+    unsigned int desp = 0;  //Acá se almacenará el valor de desplazamiento
+
+    //Averiguando el número de página de la D.V.
+    DV_TEMP >>= bits_desp;//Se eliminan los bits del desplazamientoo
+    n_pag = DV_TEMP;
+
+    //Averiguando el valor de desplazamiento
+    DV_TEMP <<= bits_desp;//Se colocan los bits de número de página en su lugar original
+    mask = DV_TEMP ^ UINT_MAX;//Con OR se forma la máscara de 11111 para poder averiguar el desplazamiento
+    desp = DV & mask;//Enmascaramiento de la dirección original para obtener desplazamiento
+
+    //Formar la dirección física
+    n_marco = getMarco(t, n_pag);//Obtener el número de marco de la tabla de páginas
+    //printf("Valor número de marco: %u", n_marco);
+    DF = n_marco << bits_desp;//Colocar el valor del marco en las posiciones correctas con corrimiento
+    DF |= desp;//Encender los bits del desplazamiento
+
+    return DF;
+}
+
+void print_table(tabla t, unsigned int size){
+    printf("|A|R|M|P|C|FRAME|\n");
+    for(int i = 0; i < size; i++){
+        printf("|%d|%d|%d|%d|%d|%u|\n",
+            t[i].b_permiso,
+            t[i].b_ref,
+            t[i].b_mod,
+            t[i].b_pres_aus,
+            t[i].b_cache,
+            t[i].n_frame);
+    }
+}
--- src/tabla_paginas.h
+++ src/tabla_paginas.h
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #ifndef TABLA_H
 #define TABLA_H
 
@@ -11,11 +12,12 @@ struct entrada{
     int b_mod;      //0 no modificada, 1 modificada
     int b_pres_aus; //0 no está en memoria principal, 1 está en memoria principal
     int b_cache;    //0 cache habilitado, 1 caché inhabilitado
-    int n_frame;
+    unsigned int n_frame;
 };
 
 int add_entry(tabla table, table_entry_t entrada, int index); //Función para añadir entradas a la tablaa
-int init_table(int size);   //Función para inicializar la tabla
-int translateVD_PD();   //Función para traducir memoria virtual a física
-
+int init_table(FILE* file, tabla* tabla_paginas, unsigned int size, unsigned int* tam_pag, unsigned int* num_pag, unsigned int* num_marcos);   //Función para inicializar la tabla
+unsigned int getMarco(tabla t, unsigned int index);   //Función para encontrar el número de marco dado un índice de página
+int translateVD_PD(tabla t, const int tam_pag, const int num_pag, const int num_marcos, const int bits_desp, unsigned int DV);   //Función para traducir memoria virtual a física
+void print_table(tabla t, unsigned int size);
 #endif
