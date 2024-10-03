22b13637bc2974eeaa1a4b01b0fdfc189058a686
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-10-03 02:34:45 -0600

    make it public

--- src/file_entries.h
+++ src/file_entries.h
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 void leer(FILE* file, tabla* t, unsigned int size, unsigned int* tam_pag, unsigned int* num_pag, unsigned int* num_marcos);
+void print_binary(unsigned long int value, unsigned int page_size);
 void generadorEntradas(FILE* file, unsigned int tam_pag, unsigned int num_pag, unsigned int num_marcos);    //Función para generar las entradas de la tabla y ponerlas en un archivo de texto
 
 #endif
