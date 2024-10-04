cc6fbaa63ab16a9b662fb8058168f7e5e6cd53f2
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-10-03 21:44:31 -0600

    file_entries no tenia declarado leer_primera_linea

--- src/file_entries.h
+++ src/file_entries.h
@@ -3,6 +3,7 @@
 #define STR_ENTR_H
 
 #include <stdio.h>
+void leer_primera_linea(FILE* file, unsigned int* tam_pag, unsigned int* num_pag, unsigned int* num_marcos);
 void leer(FILE* file, tabla* t, unsigned int size, unsigned int* tam_pag, unsigned int* num_pag, unsigned int* num_marcos);
 void print_binary(unsigned long int value, unsigned int page_size);
 void generadorEntradas(FILE* file, unsigned int tam_pag, unsigned int num_pag, unsigned int num_marcos);    //Función para generar las entradas de la tabla y ponerlas en un archivo de texto
