60200b4409959c0493525c274b37457c5feb6aa7
Rain <114127654+RainBWP@users.noreply.github.com>
2024-10-25 19:33:41 +0000

    little change to desplazamiento

--- src/main.c
+++ src/main.c
@@ -145,7 +145,7 @@ int main(int argc, char** argv){
                 bits_desp = (unsigned int) log2(tam_pag);
                 DF = translateVD_PD(page_table, tam_pag, num_pag, num_marcos, bits_desp, DV);
                 printf("\nDirección física\nEn decimal: %u \nEn hexadecimal: 0x%X \nEn binario: ", DF, DF);
-                printBinary(DF, (int) log2(num_marcos * pow(2, 5)));
+                printBinary(DF, (int) log2(num_marcos * num_pag));
             }
         }
         else if(opc == 5){
