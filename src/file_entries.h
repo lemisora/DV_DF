1587e514061eeab308fb21a793ac718460342b9e
lemisora <leonardo.michel.dom@gmail.com>
2024-10-27 16:47:08 -0600

    Adición de comentarios para cada función útil

--- src/file_entries.h
+++ src/file_entries.h
@@ -3,9 +3,14 @@
 #define STR_ENTR_H
 
 #include <stdio.h>
+
+//Función para leer los datos de la primera línea del archivo que contienen información de la tabla
 void leer_primera_linea(FILE* file, unsigned int* tam_pag, unsigned int* num_pag, unsigned int* num_marcos);
+
+//Función para leer los datos del archivo y cargarlos en memoria
 void leer(FILE* file, tabla* t, unsigned int size, unsigned int* tam_pag, unsigned int* num_pag, unsigned int* num_marcos);
-void print_binary(unsigned long int value, unsigned int page_size);
-void generadorEntradas(FILE* file, unsigned int tam_pag, unsigned int num_pag, unsigned int num_marcos);    //Función para generar las entradas de la tabla y ponerlas en un archivo de texto
+
+//Función para generar entradas de la tabla de páginas y guardarlas en un archivo de texto
+void generadorEntradas(FILE* file, unsigned int tam_pag, unsigned int num_pag, unsigned int num_marcos);
 
 #endif
