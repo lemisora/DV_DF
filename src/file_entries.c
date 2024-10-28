4149e0f4678c6b2ae2daa4f7caa2e4655e5d25a3
lemisora <leonardo.michel.dom@gmail.com>
2024-10-28 13:59:19 -0600

    Se ajusta al formato pedido en clase, con corrimiento de bits

--- src/file_entries.c
+++ src/file_entries.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 void leer_primera_linea(FILE* file, unsigned int* tam_pag, unsigned int* num_pag, unsigned int* num_marcos){
     fscanf(file, "%*s %u %*s %u %*s %u", tam_pag, num_pag, num_marcos);
 }
@@ -13,12 +13,23 @@ void leer(FILE* file, tabla* t, unsigned int size, unsigned int* tam_pag, unsign
     int b_mod = 0;
     int b_pres_aus = 0;
     int b_cache = 0;
+
+    unsigned int num = 0;
     unsigned int n_frame = 0;
+    unsigned int desp = log2(*num_marcos);
 
     fscanf(file, "%*s %u %*s %u %*s %u", tam_pag, num_pag, num_marcos);
 
     for(int i = 0; i < size; i++){
-        fscanf(file, "%d %d %d %d %d %u", &b_permiso, &b_ref, &b_mod, &b_pres_aus, &b_cache, &n_frame);
+        // fscanf(file, "%d %d %d %d %d %u", &b_permiso, &b_ref, &b_mod, &b_pres_aus, &b_cache, &n_frame);
+        fscanf(file, "%u", &num);
+        //num = (b_permiso << (desp+5)) | (b_ref << (desp+4)) | (b_mod << (desp+3)) | (b_pres_aus << (desp+2)) | (b_cache << (desp+1)) | n_frame;
+        b_permiso = (num >> (desp+5)) & 1;
+        b_ref = (num >> (desp+4)) & 1;
+        b_mod = (num >> (desp+3)) & 1;
+        b_pres_aus = (num >> (desp+2)) & 1;
+        b_cache = (num >> (desp+1)) & 1;
+        n_frame = num & desp;
         table_entry_t temp = {
                                    .b_permiso = b_permiso,
                                    .b_ref = b_ref,
@@ -35,17 +46,28 @@ void leer(FILE* file, tabla* t, unsigned int size, unsigned int* tam_pag, unsign
 void generadorEntradas(FILE* file, unsigned int tam_pag, unsigned int num_pag, unsigned int num_marcos){
     srand(time(NULL));
     int b_permiso, b_ref, b_mod, b_pres_aus, b_cache, n_frame;
+    unsigned int num = 0;
+    unsigned int desp = log2(num_marcos);
     char buffer[100];
     snprintf(buffer, sizeof(buffer), "tam_pag: %u num_pag: %u num_marcos: %u", tam_pag, num_pag, num_marcos);
     fprintf(file, "%s\n", buffer);
     for(int i = 0; i < num_pag; i++){
+        num = 0;
         b_permiso = rand() % 2;
         b_ref = rand() % 2;
         b_mod = rand() % 2;
         b_pres_aus = rand() % 2;
         b_cache = rand() % 2;
         n_frame = rand() % num_marcos;
-        fprintf(file, "%d %d %d %d %d %d\n", b_permiso, b_ref, b_mod, b_pres_aus, b_cache, n_frame);
+        // fprintf(file, "%d %d %d %d %d %d\n", b_permiso, b_ref, b_mod, b_pres_aus, b_cache, n_frame);
+        // num <<= b_permiso ; num &= b_permiso;
+        // num <<= b_ref; num &= b_ref;
+        // num <<= b_mod; num &= b_mod;
+        // num <<= b_pres_aus; num &= b_pres_aus;
+        // num <<= b_cache; num &= b_cache;
+        // num <<= n_frame; num &= n_frame;
+        num = (b_permiso << (desp+5)) | (b_ref << (desp+4)) | (b_mod << (desp+3)) | (b_pres_aus << (desp+2)) | (b_cache << (desp+1)) | n_frame;
+        fprintf(file, "%d\n", num);
     }
     fclose(file);
 }
